1ee37f1e5bd64af425e666c9135884cd3e665072
Isamar <isamarcortes589@gmail.com>
2021-09-24 16:15:07 -0400

    commit tree d48b9e3b31f57f47b4cf5e4a575a9068024954ac

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2 
 
-// Write your code here
+let random = Math.floor(Math.random() * 20)
+
+let num3 = 100
+let num4 = 12
+let mod = num3 % num4
+
+let max = Math.max(20)
